e33f9b6095716b86248b72c5e383eda7f47e2c87
tpamplin <gartiman1@gmail.com>
2025-06-11 17:32:16 -0400

    fixed styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
+
 function App() {
     return (
         <>
