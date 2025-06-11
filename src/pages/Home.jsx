f2ccd532b7e7f0fad41b4004c169afdc4ea56e2b
tpamplin <gartiman1@gmail.com>
2025-06-11 00:28:53 -0400

    create ThemeToggle.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 
 export const Home = () => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme Toggle */}
 
             {/* Background Effects */}
