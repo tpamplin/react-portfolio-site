b6fc0b8fb188d23d06d44ebe61a4e3cfa05bfd63
tpamplin <gartiman1@gmail.com>
2025-06-11 17:31:51 -0400

    updated imports

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), autoprefixer()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
