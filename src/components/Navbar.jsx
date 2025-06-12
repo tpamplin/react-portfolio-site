8184b3b6ec62a0922eadd4d40ba10351370eac15
tpamplin <gartiman1@gmail.com>
2025-06-12 19:03:19 -0400

    create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const navItems = [
+    { name: 'Home', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Skills', href: '#' },
+    { name: 'Projects', href: '#' },
+    { name: 'Contact', href: '#' }
+]
+
+export const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
