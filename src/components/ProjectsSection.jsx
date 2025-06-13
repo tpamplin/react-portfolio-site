5eacb5b28245a07bd8a073f6628a009d8de1599e
tpamplin <gartiman1@gmail.com>
2025-06-13 01:18:16 -0400

    create basic projectssection

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,26 @@
+
+const projects = [
+    {
+        title: "Project One",
+        description: "Description of project one.",
+        link: "#"
+    },
+    {
+        title: "Project Two",
+        description: "Description of project two.",
+        link: "#"
+    },
+    {
+        title: "Project Three",
+        description: "Description of project three.",
+        link: "#"
+    }
+]
+
+export const ProjectsSection = () => {
+    return (
+        <section id="projects" className="py-24 px-4 relative bg-secondary/30">
+
+        </section>
+    );
+}
