e2df0a1028cdff030c30e9f2c0f5cc930f281bb8
tpamplin <gartiman1@gmail.com>
2025-06-12 23:53:25 -0400

    add resume download button

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -21,6 +21,15 @@ export const AboutSection = () => {
                             problem-solving skills, and a commitment to delivering real 
                             value through code.
                         </p>
+
+                        <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+                            <a href="#contact" className="cosmic-button">
+                                {" "}
+                                Get In Touch
+                            </a>
+                            
+                            <a href="../../public/Timothy-Pamplin-Resume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download Resume</a>
+                        </div>
                     </div>
 
                     <div>
