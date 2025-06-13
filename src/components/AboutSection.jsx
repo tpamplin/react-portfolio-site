823a509b982fae0386b416b0baa4d5a06ee20aca
tpamplin <gartiman1@gmail.com>
2025-06-13 00:21:37 -0400

    finish About Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,3 +1,5 @@
+import { Code, Computer, Building2 } from "lucide-react";
+
 export const AboutSection = () => {
     return (
         <section id="about" className="py-24 px-4 relative">
@@ -32,8 +34,49 @@ export const AboutSection = () => {
                         </div>
                     </div>
 
-                    <div>
+                    <div className="grid grid-cols-1 gap-6">
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+                                <div className="p-3 my-auto rounded-full bg-primary/10">
+                                    <Code className="h-6 w-6 text-primary" />
+                                </div>
+                                <div className="text-center">
+                                    <h4 className="text-primary font-semibold"> Web Development </h4>
+                                    <p>
+                                        Building responsive, user-friendly web applications using modern frameworks.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+                                <div className="p-3  my-auto rounded-full bg-primary/10">
+                                    <Computer className="h-6 w-6 text-primary"  />
+                                </div>
+
+                                <div className="text-center">
+                                    <h4 className="text-primary font-semibold"> User Experience </h4>
+                                    <p>
+                                        Focusing on creating intuitive and engaging user experiences through design and functionality.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+                                <div className="p-3  my-auto rounded-full bg-primary/10">
+                                    <Building2 className="h-6 w-6 text-primary"  />
+                                </div>
+                                <div className="text-center">
+                                    <h4 className="text-primary font-semibold">Team Collaboration</h4>
+                                    <p>
+                                        Working effectively in teams, contributing to projects with a collaborative spirit.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
             
                 </div>
