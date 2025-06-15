1c9812768564bbe6dd50892c02597d4efe313f56
tpamplin <gartiman1@gmail.com>
2025-06-15 13:26:30 -0400

    fix link?

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                                 Get In Touch
                             </a>
                             
-                            <a href="../../public/Timothy-Pamplin-Resume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download Resume</a>
+                            <a href="/Timothy-Pamplin-Resume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download Resume</a>
                         </div>
                     </div>
 
