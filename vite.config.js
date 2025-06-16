180f63d87b50d6c87a6613b35e2c7cdcab209cb5
tpamplin <gartiman1@gmail.com>
2025-06-16 15:11:33 -0400

    Showing Case studies as pdfs

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,7 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+        extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx"],
     },
+    base: "./",
 });
